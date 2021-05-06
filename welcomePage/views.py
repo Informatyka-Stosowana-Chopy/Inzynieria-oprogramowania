b92bc39b293baaae13c7d139a8b9fcee7863c8f2
ksiazeblaznow <ksiazeblaznow@gmail.com>
2021-05-07 00:47:53 +0200

    sciezka bezwzgledna na wzgledna tylko
    nihil grande

--- welcomePage/views.py
+++ welcomePage/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     s = ""
-    with open(r"D:\programowanie\python\Inzynieria_oprogramowania\welcomePage\enter.html", "r") as f:
+    with open(r"welcomePage\enter.html", "r") as f:
         s = f.readlines()
     return HttpResponse(s)
+
