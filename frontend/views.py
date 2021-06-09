635a2d6f4f8da953542496579fef21730068726d
Michal Olczak <micolo2000@gmail.com>
2021-06-10 00:53:29 +0200

    user login page

--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def index(request, *args, **kwargs):
     if request.method == 'POST':
         text = request.POST.get('input')
+        username = request.POST.get('input-login')
+        password = request.POST.get('input-password')
+        user = authenticate(request, username=username, password=password)
+        print(type(user))
+        if user is not None:
+            login(request, user)
+        if request.user.is_authenticated:
+            name = 'imie' + request.user.username
+            print(name)
+            return render(request, 'frontend/index.html', {'name': name, 'text': text, 'user': user})
     else:
         text = 'placeholder'
     if request.user.is_authenticated:
