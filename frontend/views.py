d7b2c9a2f3b33aae00162ab98924967e2e7d7684
Michal Olczak <micolo2000@gmail.com>
2021-06-09 22:00:03 +0200

    user authentication

--- frontend/views.py
+++ frontend/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request, *args, **kwargs):
-    return render(request, 'frontend/index.html')
+    if request.method == 'POST':
+        text = request.POST.get('input')
+    else:
+        text = 'placeholder'
+    if request.user.is_authenticated:
+        name = 'imie' + request.user.username
+        print(name)
+        return render(request, 'frontend/index.html', {'name': name, 'text': text})
+    else:
+        name = 'blank_user'
+    return render(request, 'frontend/index.html', {'name': name, 'text': text})
